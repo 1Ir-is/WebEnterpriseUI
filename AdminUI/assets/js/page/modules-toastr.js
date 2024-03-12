80df149958b0421648b6205f586a3c32166cb07a
1Ir-is <mhuynk1005@gmail.com>
2024-03-12 16:05:55 +0700

    feat: add toast and alert for the function

--- AdminUI/assets/js/page/modules-toastr.js
+++ AdminUI/assets/js/page/modules-toastr.js
@@ -1,65 +1,21 @@
 "use strict";
 
-$("#toastr-1").click(function() {
-  iziToast.info({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made iziToast toastr',
-    position: 'topRight'
-  });
-});
+$("#toastr-2").click(function () {
+  // Check if the form is valid
+  if ($("#academicYearForm")[0].checkValidity()) {
+      // Your code to handle the form submission
+      iziToast.success({
+          title: 'Success!',
+          message: 'Academic year created successfully!',
+          position: 'topRight'
+      });
 
-$("#toastr-2").click(function() {
-  iziToast.success({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'topRight'
-  });
-});
-
-$("#toastr-3").click(function() {
-  iziToast.warning({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'topRight'
-  });
-});
-
-$("#toastr-4").click(function() {
-  iziToast.error({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'topRight'
-  });
-});
-
-$("#toastr-5").click(function() {
-  iziToast.show({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'bottomRight' 
-  });
-});
-
-$("#toastr-6").click(function() {
-  iziToast.show({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'bottomCenter' 
-  });
-});
-
-$("#toastr-7").click(function() {
-  iziToast.show({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'bottomLeft' 
-  });
-});
-
-$("#toastr-8").click(function() {
-  iziToast.show({
-    title: 'Hello, world!',
-    message: 'This awesome plugin is made by iziToast',
-    position: 'topCenter' 
-  });
+      // Redirect to admin.html after 3000 milliseconds (3 seconds)
+      setTimeout(function () {
+          window.location.href = 'admin.html';
+      }, 3000);
+  } else {
+      // If the form is not valid, trigger form validation
+      $("#academicYearForm").addClass('was-validated');
+  }
 });
